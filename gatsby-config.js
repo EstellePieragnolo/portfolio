f2d9a2b485d0eb28b3674534191313db3100d62c
Esteban <estellepieragnolo@gmail.com>
2019-01-05 23:14:45 +0100

    add sassé

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: "Estelle Pieragnolo",
+    description: "Estelle Pieragnolo - portfolio",
+    author: "Estelle Pieragnolo",
+    siteUrl: ""
+  },
+  plugins: [`gatsby-plugin-sass`]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "../component/Home";
-import "./index.css";
+import "./index.scss";
 
 export default class Index extends React.Component {
   render() {
